ffbdd37e2652c297a6ceb2a04b6e16d868a17b3f
wistromh04 <wherfordt@gmail.com>
2024-06-26 21:32:44 -0500

    Added color change

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let color = "black";
+
 function setupBoard(size) {
     let board = document.querySelector(".board");
     board.innerHTML = ''; //clears existing squares
@@ -7,10 +9,8 @@ function setupBoard(size) {
 
     for (let i = 0; i < amount; i++) {
         let square = document.createElement("div");
-        square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = "black";
-        })
-        square.style.backgroundColor = "blue";
+        square.addEventListener("mouseover", colorSquare)
+        square.style.backgroundColor = "white";
         board.insertAdjacentElement("beforeend", square)
     }
 }
@@ -23,4 +23,17 @@ function changeSize(input) {
     } else {
         console.log("error");
     }
+}
+
+function colorSquare(){
+    if (color === "random"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+
+    }else {
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice){
+color = choice;
 }
